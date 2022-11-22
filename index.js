ae5edb03aeae5d034b2f91375d9be5901f3ce6b1
Zhiyuan Ma <631253076@qq.com>
2022-11-22 04:20:56 -0500

    Select button and click to remove black friday banner

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('https://leetcode.com/hongsuzu/');
     await page.waitForTimeout(10000);
+    const button = await page.$("#__next > div > div.flex.w-full.flex-col.z-nav > div > div.pointer-events-none.absolute.top-0.left-0.flex.h-full.w-full.justify-center.px-5 > div > button");
+    if (button) {
+        await button.click();
+    }
     await page.screenshot({path: 'lc.png', clip: {x: 1018, y: 72, width: 421, height: 204}});
     console.log(new Date().toUTCString());
     await browser.close();
